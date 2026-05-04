d863c56fc6821634dca2e13414076a65bcff8a38
Anna Wester <annawester73@gmail.com>
2026-05-04 19:57:21 +0200

    Add id to books in function addBook

--- src/bookstore.py
+++ src/bookstore.py
@@ -1,10 +1,13 @@
 class BookStore:
     def __init__(self):
         self.books = []
+        self.next_id = 1
 
     def addBook(self, author, title):
-        book = {"author": author,"title": title}
+        book = {"id": self.next_id,"author": author,"title": title}
 
         self.books.append(book)
+        self.next_id += 1
+
         return book
 
